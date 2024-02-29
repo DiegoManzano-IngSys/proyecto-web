d3c17457f71ef5a3e6c6c14052787bab25b4930d
Diego De Jesús Manzano Chávez <107444647+DiegoManzano-IngSys@users.noreply.github.com>
2024-02-28 22:55:09 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> SI JALO.
+          Edit <code>src/App.js</code> SI JALO no jalo.
         </p>
         <a
           className="App-link"
