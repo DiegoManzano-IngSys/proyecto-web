2cf2cb62c95ff388333fa128273c0aa2c2e82e3c
Diego De Jesús Manzano Chávez <107444647+DiegoManzano-IngSys@users.noreply.github.com>
2024-02-28 23:07:34 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> HO.
-          Edit <code>src/App.js</code>Lo logré.
+          Edit <code>src/App.js</code>Lo logré :).
         </p>
         <a
           className="App-link"
